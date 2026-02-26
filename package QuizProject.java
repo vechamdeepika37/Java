d1f5d5aa3558b57708250944e558f58764dedfa1
vechamdeepika37 <vechamdeepika@gmail.com>
2026-02-26 13:26:53 +0530

    package QuizProject.java

---
+++ package QuizProject.java
@@ -0,0 +1,26 @@
+package Quiz;
+
+
+class Question {
+    private String questionText;
+    private String[] options;
+    private int correctAnswer;
+
+    public Question(String questionText, String[] options, int correctAnswer) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+}
